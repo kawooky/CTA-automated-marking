3ff4dcc017bcd2e7217af5ee21ec00028d8735c6
kawooky <kawooky@gmail.com>
2024-09-27 15:04:00 +0100

    adds to teh bottom of the results list

--- logging_utils.py
+++ logging_utils.py
@@ -1,19 +1,29 @@
 import pandas as pd
 import os
+from openpyxl import load_workbook
 
 def log_results_to_excel(results, output_file, append=False):
     df = pd.DataFrame(results)  # Convert results to DataFrame
 
     if append and os.path.exists(output_file):
-        # If append mode is selected, open the Excel file in append mode
+        # Open the workbook and find the last row of the 'Results' sheet
         with pd.ExcelWriter(output_file, mode='a', engine='openpyxl', if_sheet_exists='overlay') as writer:
-            try:
-                # Try to access the existing 'Results' sheet
-                startrow = writer.sheets['Results'].max_row  # Get the last row of the 'Results' sheet
-                df.to_excel(writer, index=False, sheet_name='Results', startrow=startrow, header=False)  # Append without header
-            except KeyError:
-                # If 'Results' sheet does not exist, create it and write the DataFrame
+            workbook = load_workbook(output_file)
+            if 'Results' in workbook.sheetnames:
+                worksheet = workbook['Results']
+
+                # Find the last non-empty row (ignoring any empty rows below the data)
+                startrow = worksheet.max_row
+                for row in worksheet.iter_rows(min_row=worksheet.max_row, max_row=worksheet.max_row):
+                    if all([cell.value is None for cell in row]):
+                        startrow -= 1  # If last row is empty, adjust the startrow
+
+                # Append new data below the last non-empty row
+                df.to_excel(writer, index=False, sheet_name='Results', startrow=startrow, header=False)
+            else:
+                # If 'Results' sheet doesn't exist, create it and write the DataFrame
                 df.to_excel(writer, index=False, sheet_name='Results')  # Write to new 'Results' sheet
     else:
-        # Otherwise, create a new Excel file
+        # If append is False, create a new Excel file
         df.to_excel(output_file, index=False, sheet_name='Results')
+
